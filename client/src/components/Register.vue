8e9c3c6e446a994db3e2ebcfa0ef577708511584
Krishna Solanki <kasolanki06@gmail.com>
2019-04-17 21:25:29 -0400

    started using vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,25 +1,25 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="orange" dark>
+          <v-toolbar-title dark>Register</v-toolbar-title>
+        </v-toolbar>
 
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
-</template>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <form name="fitness-tracker-form" autocomplete="off">
+            <v-text-field label="Email" v-model="email"></v-text-field>
+            <br>
+            <v-text-field label="Password" type="password" v-model="password" autocomplete="new-password"></v-text-field>
+          </form>
+          <br>
+          <div class="error" v-html="error" /><br>
+          <v-btn dark class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+ </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
@@ -27,15 +27,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -43,5 +48,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error{
+  color: red;
+}
 </style>
