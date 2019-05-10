af51a9c0f08dfc72565e9846d2448329e8ee1bbc
Krishna Solanki <kasolanki06@gmail.com>
2019-05-09 23:55:45 -0400

    song features added

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,6 +2,8 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 const SongsController = require('./controllers/SongsController')
 const BookmarksController = require('./controllers/BookmarksController')
+const HistoriesController = require('./controllers/HistoriesController')
+const isAuthenticated = require('./policies/isAuthenticated')
 
 module.exports = (app) => {
   app.post('/register',
@@ -17,14 +19,20 @@ module.exports = (app) => {
   app.post('/songs',
       SongsController.post)
 
-  app.get('/bookmarks',
+  app.get('/bookmarks', isAuthenticated,
     BookmarksController.index)
 
-  app.post('/bookmarks',
+  app.post('/bookmarks', isAuthenticated,
     BookmarksController.post)
 
-  app.delete('/bookmarks/:bookmarkId',
+  app.delete('/bookmarks/:bookmarkId', isAuthenticated,
     BookmarksController.delete)
+
+  app.get('/histories', isAuthenticated,
+    HistoriesController.index)
+    
+  app.post('/histories', isAuthenticated,
+    HistoriesController.post)
 
   app.get('/songs',
     SongsController.index)
